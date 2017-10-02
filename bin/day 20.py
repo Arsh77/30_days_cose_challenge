bb046e77c67681b1ec7eb467a4551419b7f77185
Arsh77 <31885057+Arsh77@users.noreply.github.com>
2017-10-02 13:37:31 +0530

    Create day 20.py

---
+++ bin/day 20.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+# Write Your Code Here
+swap = 0
+a1=0
+for i in range(n):
+    for j in range(n-1):
+        if a[j]>a[j+1]:
+            a1=a[j]
+            a[j]=a[j+1]
+            a[j+1]=a1
+            swap+=1
+    if swap==0:
+        break
+print('Array is sorted in',swap,'swaps.')        
+print('First Element:',a[0])
+print('Last Element:',a[-1])
+
+        
